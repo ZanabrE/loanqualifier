572d7af2dc3bc9981790e464d16f10b106e94364
G:\Git <ernesto.zanabria1979@gmail.com>
2022-10-10 21:52:04 -0400

    New file loanqualifier.class
    Modified loanqualifier.java

--- loanqualifier.java
+++ loanqualifier.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
 
  public class loanqualifier{
     public static void main(String[] args) {
+        //Header.
+        System.out.print("****************************************\n");
+        System.out.print("*          Loan Qualifier App          *\n");
+        System.out.print("****************************************\n");
+        
         //Variables.
         double salary;
         double yearsOnJob;
@@ -15,12 +20,19 @@ import java.util.Scanner;
         Scanner keyboard = new Scanner(System.in);
 
         //Get the users salary.
-        System.out.println("Enter you annual salary: \n");
+        System.out.println("Enter you annual salary: ");
         salary = keyboard.nextDouble();
 
         //Get the user's years on the job.
-        System.out.println("Enter the years work for your current company: \n");
+        System.out.println("Enter the years work for your current company: ");
         yearsOnJob = keyboard.nextDouble();
         
+        //Determine whether the user qualify for the loan.
+        if(salary >= 50000 || yearsOnJob >= 2){
+            System.out.println("You qualify for the loan.");
+        }
+        else{
+            System.out.println("Sorry, you don't qualify for the loan.");
+        }
     }
  }
